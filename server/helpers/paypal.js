e2d814b7a792c6f90ac053ec714818ab614706c8
Sehajsaini <155537305+Sehajsaini@users.noreply.github.com>
2025-07-18 18:06:32 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "AQjbfwjef-aqWnsJJ9nQP_KE8-FWsYzJKA9RLXZYpZRPUCCyRyGMHr4R5XlQb9DiWpQu9YsY0SuUR3rx",
-  client_secret: "EHN7eWS_pGvJ5bwgutHd-gd498smyr6TpVDqP-VoQsTiTKPq3IM5ZvwNyGSyXEsFX2DMCF53GVhICicS",
+  client_id: " ",
+  client_secret: " ",
 });
 
 module.exports = paypal;
